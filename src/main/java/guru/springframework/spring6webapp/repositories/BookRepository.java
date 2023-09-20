7160b8b401a166cdd89b8ee557d19ae93fbe4021
Jaakko Kotitalo <jaakko.kotitalo@vincit.fi>
2023-09-20 19:50:52 +0300

    Add Author and Book repositories

---
+++ src/main/java/guru/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6webapp.repositories;
+
+import guru.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long>{
+}
